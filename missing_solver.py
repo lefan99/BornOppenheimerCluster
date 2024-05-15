deleted plot script , plotting through jupyter notebook via VS code is simpler, fixed sign error in potential in dot potential. Paths file to store the paths of the saved vectors etc. as python string for simplifying the changing process

missing_solver.py
@@ -8,8 +8,8 @@ missing_y = np.load('../missing_y.npy')
 
 
 def main():
-    index = int(sys.argv[1]) * 100
-    for i in range(100): 
+    index = int(sys.argv[1]) * 7
+    for i in range(7): 
         if index+i in range(len(missing_x)):
             solver.solver( missing_x[index+i] , missing_y[index+i]  , 0)
             print( ' calc done for xcom ='  , missing_x[index+i] , 'and ycom' , missing_y[index+i] , 'given as index of BO_array') 

parameters.py
@@ -43,7 +43,7 @@ potential_mode = 'dot'
 if potential_mode == 'erf':
     #fields = np.arange(0e-3, 1025e-3, 25e-3)
     #fields = np.round(fields, decimals=3)
-    fields = [425e-3]
+    fields = [425e-3 , 0.725]
     sigma = [20e-9 * np.sqrt(2)]*len(fields)
 
 if potential_mode == 'interp':
@@ -51,7 +51,7 @@ if potential_mode == 'interp':
     potential_index = [0]
 
 if potential_mode == 'dot':
-    fields = [425e-3]
+    fields = [425e-3, 0.725]
     sigma = [20e-9 * np.sqrt(2)]*len(fields)
     
 

pos_solver.py
@@ -115,57 +115,62 @@ class Hamiltonian():
 GRID_1 = grid(para.m , para.x_width , para.n , para.y_width , 1 , 0 , 1 ,0)
 
 #Import the data from the COM calculations, convention follows the local version written by timo 
-BO_energy = []
-BO_states = np.zeros([para.m , para.n , para.o])
+BO_energy = np.zeros([para.o , para.o]) 
+BO_states = np.zeros([para.m , para.n , para.o , para.o])
 BO_array = np.linspace(-para.com_width, para.com_width, para.o, endpoint=True)
-f_x = 0.4
+f_x = 0.425
 pot_index = int(sys.argv[1])
-for j in range(len(BO_array)):
+for i , x in enumerate( BO_array):
 
-   i = BO_array[j]
+    for j , y in enumerate(BO_array):
 
-   if os.path.exists('../hamiltonian/rel_data/states/pot{}/com_x{}_y{}.npy'.format(para.fields[pot_index], i, BO_array[0])):
+        if os.path.exists('../hamiltonian/rel_data/states/pot{}/com_x{}_y{}.npy'.format(para.fields[pot_index], x, y)):
        
-        BO_energy.append( np.load('../hamiltonian/rel_data/energies/pot{}/com_x{}_y{}.npy'.format(para.fields[pot_index], i,  BO_array[0])) )
-        state = np.load('../hamiltonian/rel_data/states/pot{}/com_x{}_y{}.npy'.format(para.fields[pot_index], i, BO_array[0]))
+            BO_energy[ i , j] = np.load('../hamiltonian/rel_data/energies/pot{}/com_x{}_y{}.npy'.format(para.fields[pot_index], x,  y)) 
+            state = np.load('../hamiltonian/rel_data/states/pot{}/com_x{}_y{}.npy'.format(para.fields[pot_index], x, y ))
 
-        BO_states[:,:,j] = state * np.sign(state)
-
+            BO_states[:,:, i , j] = state * np.sign(state)
+print('finished loading')
 
 #print(len(BO_energy) , len(BO_states))
-BO_energy = np.asarray( BO_energy)
+#BO_energy = np.asarray( BO_energy)
 #print('energy ' ,BO_energy ,'states', BO_states) 
 
 #set up grid and hamiltonian to solve the positional schrodinger equation
 
-GRID = grid(1, 0 ,1 ,0 , para.o, para.com_width , 1, 0)
+plt.imshow(BO_energy, cmap = 'autumn' ,interpolation = 'gaussian') 
+plt.savefig('dot_confine_surface.pdf')
+print('start building Hamiltonian')
+
+GRID = grid(1, 0 ,1 ,0 , para.o, para.com_width , para.o, para.com_width )
 Ham = laplacian(GRID)
-V_BO = diags(BO_energy)
+V_BO = diags(BO_energy.flatten())
 
 
 #print( 'HKIN' , Ham.Hkin, 'potential'  , V_BO ,'bo energy surface',  BO_energy)
 H = Ham.Hkin + V_BO
 energies, states = eigsh(H , k = 10, which='SA') 
 
-print(energies/para.joul_to_eV)
+print('finished!')
 
 #plot the BO energy surface
-fig, ax = plt.subplots()
-ax.plot(BO_array*1e9, BO_energy/para.joul_to_eV)
-ax.axhline(energies[0]/para.joul_to_eV)
+#fig, ax = plt.subplots()
+#ax.plot(BO_array*1e9, BO_energy/para.joul_to_eV)
+#ax.axhline(energies[0]/para.joul_to_eV)
 
 
+#for i in energies:
 
-for i in energies:
-
-    ax.axhline(i/para.joul_to_eV)
-fig.savefig('../plots/COM_potential/test_fx_{}.pdf'.format(pot_index))
+#   ax.axhline(i/para.joul_to_eV)
+#plt.show()
+#fig.savefig('../plots/COM_potential/dot_confine_fx_{}.pdf'.format(pot_index))
 
 
 #calculate the final state as a product of the POS and REL state
-shape = (para.m, para.n , para.o , 1) 
+shape = (para.m, para.n , para.o , para.o, 1) 
 BO_states = np.reshape(BO_states , newshape = (shape) )
-shape_states = ( 1, 1 , para.o , 10 ) 
+shape_states = ( 1, 1 , para.o , para.o, 10 ) 
+states = np.reshape( states , newshape = (shape_states)) 
 states = BO_states * states
 
 #Normalize the exciton wave function using trapezoidal integration.
@@ -176,7 +181,7 @@ normalize = np.trapz(normalize, GRID_1.x, axis=0)
 states = states / np.sqrt(normalize)
 
 #Save the normalized, sorted and processed states for further use. One can do any further postprocessing with them now.
-Os.makedirs('../hamiltonian/statesData/', exist_ok=True)
+os.makedirs('../hamiltonian/statesData/', exist_ok=True)
 os.makedirs('../hamiltonian/energiesData/', exist_ok=True)
 np.save('../hamiltonian/statesData/V_0={}'.format(f_x), states)
 np.save('../hamiltonian/energiesData/V_0={}'.format(f_x), energies)
@@ -190,14 +195,14 @@ k_l_square = k_l_square / 1e9
 
 #Make a plot of the obtained lowest lying motional state at relative coordinate r=0.
 fig, ax = plt.subplots()
-ax.plot(BO_array / 1e-9, states[int(para.m/2), int(para.n/2), :, 0] * (1e-9)**(3/2), label='f_x={}'.format(f_x))
+#ax.plot(BO_array / 1e-9, states[int(para.m/2), int(para.n/2), :, 0] * (1e-9)**(3/2), label='f_x={}'.format(f_x))
+ax.imshow( states[ 0 , 0 , : , : ,0]**2 , interpolation='gaussian')
 ax.legend()
 ax.grid(True)
 ax.set_ylabel(r'$|\phi(X,r=0)|$ [1/$nm^(3/2)$]')
 ax.set_xlabel(r'X [nm]')
 plt.tight_layout()
-plt.show()
-fig.savefig('../plots/COM_groundstate/mls_at_overlap_fx_{}_generalized.pdf'.format(f_x))
+fig.savefig('mls_at_overlap_dot.pdf'.format(f_x))
 
 #Plot the field dependence of the oscillator strength density.
 #fig, ax = plt.subplots()

solver.py
@@ -124,11 +124,11 @@ class laplacian():
                         
 
                         if ycom_index+1 != dimensions[3]: #Checking for upper bound
-                            origin_right_ycom_neighbour = np.ravel_multi_index((x_index, y_index, xcom_index+1, ycom_index ), dimensions, mode='raise')
+                            origin_right_ycom_neighbour = np.ravel_multi_index((x_index, y_index, xcom_index, ycom_index+1 ), dimensions, mode='raise')
                             self._append((1 / grid.dycom**2 *(-para.h_bar**2/(2*para.M))), origin, origin_right_ycom_neighbour)
 
                         if ycom_index-1 != -1: #Checking for lower bound
-                            origin_left_ycom_neighbour  = np.ravel_multi_index((x_index, y_index, xcom_index-1, ycom_index ), dimensions, mode='raise')
+                            origin_left_ycom_neighbour  = np.ravel_multi_index((x_index, y_index, xcom_index, ycom_index-1 ), dimensions, mode='raise')
                             self._append((1 / grid.dycom**2 *(-para.h_bar**2/(2*para.M))), origin, origin_left_ycom_neighbour)
 
 
@@ -182,7 +182,7 @@ class solver():
         #0D confinement case using the erf with a gaussain envelope in the Y axis
         if para.potential_mode == 'dot':
             self.V_el_conf = diags(-para.e * conf.V_dot( self.current_pot , para.m_valence/para.M*self.GRID.X.reshape(-1) + self.current_xcom, para.m_valence/para.M*self.GRID.Y.reshape(-1) + self.current_ycom, self.sigma , self.sigma))
-            self.V_hl_conf = diags(-para.e * conf.V_dot( self.current_pot , -para.m_conduction/para.M*self.GRID.X.reshape(-1) + self.current_xcom, -para.m_conduction/para.M*self.GRID.Y.reshape(-1) + self.current_ycom, self.sigma , self.sigma))
+            self.V_hl_conf = diags(para.e * conf.V_dot( self.current_pot , -para.m_conduction/para.M*self.GRID.X.reshape(-1) + self.current_xcom, -para.m_conduction/para.M*self.GRID.Y.reshape(-1) + self.current_ycom, self.sigma , self.sigma))
 
     
     def _solve(self):
@@ -265,7 +265,7 @@ def main():
     #0D confinement case using the erf with a gaussain envelope in the Y axis
     if para.potential_mode == 'dot':
         V_el_conf = diags(-para.e * conf.V_dot( para.fields[current_pot] , para.m_valence/para.M*GRID.X.reshape(-1) + current_xcom, para.m_valence/para.M*GRID.X.reshape(-1) + current_xcom, para.sigma[current_pot] , para.sigma[current_pot]))
-        V_hl_conf = diags(-para.e * conf.V_dot( para.fields[current_pot] , -para.m_conduction/para.M*GRID.Y.reshape(-1) + current_ycom, -para.m_conduction/para.M*GRID.Y.reshape(-1) + current_ycom, para.sigma[current_pot] , para.sigma[current_pot]))
+        V_hl_conf = diags(para.e * conf.V_dot( para.fields[current_pot] , -para.m_conduction/para.M*GRID.Y.reshape(-1) + current_ycom, -para.m_conduction/para.M*GRID.Y.reshape(-1) + current_ycom, para.sigma[current_pot] , para.sigma[current_pot]))
         Ham = LAP.Hkin + V_keyldish + V_el_conf + V_hl_conf
 
 